96c4b1824d8f1557e5946400fbd138723778faa2
SohailAli9 <salsamahi@mail.csuchico.edu>
2017-11-06 23:55:46 -0800

    CompositeImage

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Declare the variables to store number of files, filename, and flag variables 
+// Using loop, take input from the user, the user will enter filename 
+// and store the file names. 
+ 
+// During inputting the filenames, the project will check when the user is DONE
+// So no more input will be taken by user.  
+
+
+// This function will return true or false and tell the file is valid or not.
+// It checks the existence of file, valid bitmap and dimensions.
+// bool checkFiles(string file)
+
+
+// checkFileDimension() function will return true or false and tell the file dimesnions are valid for making composition
+// or not.
+// bool checkFileDimension(string filename) 
+
+
+// combine() functions combines all the checked images to one composite image by calculating 
+// Red Green Blue component of each pixel of all valid files. 
+// combine() function not return anything.
+// void combine(bitmap)
+
+// Finally, Save the File.
